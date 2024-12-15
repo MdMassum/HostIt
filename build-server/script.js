2707ea0e8c431f92c6728bffd6084911cfa52268
Md Emamudin <mdemamudin726@gmail.com>
2024-12-15 12:20:20 +0530

    Done with Build Server

--- build-server/script.js
+++ build-server/script.js
@@ -1,5 +1,18 @@
 const {exec} = require('child_process')
 const path = require('path')
+const fs = require('fs')  // file system for reading files content
+const { PutObjectCommand, S3Client } = require('@aws-sdk/client-s3')
+const mime = require('mime-type') // it tells us type of file by the help of pathname
+
+const s3Client = new S3Client({
+    region:'',
+    credentials:{
+        accessKeyId:'',
+        secretAccessKey:''
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID
 
 async function init(){
     console.log("Executing...")
@@ -18,5 +31,25 @@ async function init(){
 
     p.on('close', async function () {
         console.log("Build Complete")
+
+        const distFolderPath = path.join(__dirname, 'output', 'dist')   // this will get the path of dist folder of build project
+        const distFolderContent = fs.readdirSync(distFolderPath,{recursive:true})  // it has all contents of dist folder eg. .html, .css , .js file
+
+        for(const file of distFolderContent){
+            const filePath = path.join(__dirname,distFolderPath,file)
+            if(fs.lstatSync(filePath).isDirectory()) continue;  // if it is a folder file let it go
+
+            console.log("uploading ",filePath)
+            const command = new PutObjectCommand({
+                Bucket:'',
+                key:`__outputs/${PROJECT_ID}/${file}`,
+                body: fs.createReadStream(filePath),
+                content: mime.lookup(filePath)
+            })
+
+            await s3Client.send(command)  // will upload all files into s3 storage
+            console.log("uploaded ",filePath)
+        }
+        console.log("Done...")
     })
 }
