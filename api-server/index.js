8ccaa64bd7086beb8b640f1614eaa2cfe5e0c05d
Md Emamudin <mdemamudin726@gmail.com>
2024-12-16 12:24:56 +0530

    Added Realtime logs with socket io and Redis

--- api-server/index.js
+++ api-server/index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const {generateSlug} = require('random-word-slugs')
 const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs')
+const { Server } = require('socket.io')
+const Redis = require('ioredis')
 
 const app = express()
 const PORT = 9000;
 
 app.use(express.json());
+
+// redis
+const subscriber = new Redis(process.env.Redish_URL); // use aiven website
+
+// socket for publishing logs realtime
+const io = new Server({cors:'*'});
+
+io.on('connection',socket=>{
+    socket.on('subscribe', channel =>{
+        socket.join(channel)
+        socket.emit('message',`Joined ${channel}`)
+    })
+})
+
+io.listen(9001, ()=> console.log("Socket Server 9001"))
 
 const ecsClient = new ECSClient({
     region: 'ap-south-1',
@@ -55,6 +72,17 @@ app.post('/project', async (req, res) => {
     return res.json({ status: 'queued', data: { projectSlug, url: `http://${projectSlug}.localhost:8000` } })
 })
 
+
+async function initRedisSubscribe() {
+    console.log('Subscribed to logs....')
+    subscriber.psubscribe('logs:*')
+    subscriber.on('pmessage', (pattern, channel, message) => {
+        io.to(channel).emit('message', message)
+    })
+}
+
+
+initRedisSubscribe()
 
 app.listen(PORT, ()=>{
     console.log(`api server Running on Port.. ${PORT}`)
--- build-server/script.js
+++ build-server/script.js
@@ -3,6 +3,9 @@ const path = require('path')
 const fs = require('fs')  // file system for reading files content
 const { PutObjectCommand, S3Client } = require('@aws-sdk/client-s3')
 const mime = require('mime-type') // it tells us type of file by the help of pathname
+const Redis = require('ioredis')
+
+const publisher = new Redis(process.env.Redish_URL); // use aiven website
 
 const s3Client = new S3Client({
     region: 'ap-south-1',
@@ -14,8 +17,14 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID
 
+// we are storing logs in redis
+function publishLog(log) {
+    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
+}
+
 async function init(){
     console.log("Executing...")
+    publishLog('Build Started...')
 
     const outDirPath = path.join(__dirname,'output')  // this will get the path of uploaded project
 
@@ -23,23 +32,28 @@ async function init(){
 
     p.stdout.on('data', function (data) { // this prints all the logs while building
         console.log(data.toString())
+        publishLog(data.toString())
     })
 
     p.stdout.on('error', function (data) {  // this prints all the error logs while building
         console.log('Error', data.toString())
+        publishLog(`error: ${data.toString()}`)
     })
 
     p.on('close', async function () {
         console.log("Build Complete")
+        publishLog(`Build Complete`)
 
         const distFolderPath = path.join(__dirname, 'output', 'dist')   // this will get the path of dist folder of build project
         const distFolderContent = fs.readdirSync(distFolderPath,{recursive:true})  // it has all contents of dist folder eg. .html, .css , .js file
 
+        publishLog(`Starting to upload`)
         for(const file of distFolderContent){
             const filePath = path.join(__dirname,distFolderPath,file)
             if(fs.lstatSync(filePath).isDirectory()) continue;  // if it is a folder file let it go
 
             console.log("uploading ",filePath)
+            publishLog(`uploading ${file}`)
             const command = new PutObjectCommand({
                 Bucket:'',
                 key:`__outputs/${PROJECT_ID}/${file}`,
@@ -49,7 +63,12 @@ async function init(){
 
             await s3Client.send(command)  // will upload all files into s3 storage
             console.log("uploaded ",filePath)
+            publishLog(`uploaded ${file}`)
+            
         }
+        publishLog(`Done`)
         console.log("Done...")
     })
 }
+
+init()
